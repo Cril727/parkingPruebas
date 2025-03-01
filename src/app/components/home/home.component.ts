7a64d009600a983c595b82cfa5ad8719961896d5
Cril727 <80360340+Cril727@users.noreply.github.com>
2025-03-01 15:14:19 -0500

    Primer segundo

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { NavBarComponent } from '../nav-bar/nav-bar.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [NavBarComponent],
+  imports: [NavBarComponent,RouterModule],
   standalone: true,
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
